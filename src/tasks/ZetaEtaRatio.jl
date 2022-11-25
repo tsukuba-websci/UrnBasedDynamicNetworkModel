7f06529c3ab8b57e52658afdb139626bc0ab2eee
SUDA Mikihrio <s@sudame.net>
2022-11-25 17:18:39 +0900

    temp

--- src/tasks/ZetaEtaRatio.jl
+++ src/tasks/ZetaEtaRatio.jl
@@ -11,7 +11,7 @@ zetas = 0:0.005:1
 
 p = Progress(length(zetas); showspeed=true)
 Threads.@threads for zeta in zetas
-    env, labels, label_tree = run_waves_model(10, 9, zeta, 0.1; steps=20000)
+    env, labels, label_tree = run_waves_model(2, 16, zeta, 0.1; steps=20000)
 
     zetastr = replace(string(zeta), ("." => "-"))
     rename(DataFrame(env.history), [:1 => :src, :2 => :dst]) |>
