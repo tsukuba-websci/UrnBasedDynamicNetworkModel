f48cfcc2f1be21fd36ddce2ae18f494afd90c127
SUDA Mikihrio <s@sudame.net>
2022-11-25 17:05:27 +0900

    temp

---
+++ src/tasks/AnalyzeZetaEtaRatio.jl
@@ -0,0 +1,20 @@
+using DataFrames, CSV
+using ProgressMeter
+
+include("../Calc.jl")
+
+eta = 0.1
+zetas = 0:0.005:1
+
+mvs = MeasuredValues[]
+p = Progress(length(zetas); showspeed=true)
+for zeta in zetas
+    zeta_str = replace(string(zeta), "." => "-")
+    df = DataFrame(CSV.File("results/zeta_eta_ratio/history/$zeta_str.csv"))
+    history = Tuple.(eachrow(df))
+    mv = MeasuredValues(history, ModelParams(2, 16, zeta, eta))
+    push!(mvs, mv)
+    next!(p)
+end
+
+DataFrame(mvs) |> CSV.write("results/zeta_eta_ratio/analyzed.csv")
