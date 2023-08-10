b9c749e0229bb939286db02ffde74f602f3cc4be
Nanami <88866882+krmr73@users.noreply.github.com>
2023-08-10 16:53:39 +0900

    update: ベースモデルを2種類の戦略で走らせるように変更

--- src/tasks/RunBaseModel.jl
+++ src/tasks/RunBaseModel.jl
@@ -24,48 +24,55 @@ function exec()
 
     rhos = 1:20 |> collect
     nus = 1:20 |> collect
+    ss = ("asw", "wsw")
     zeta = 0
     eta = 0
 
-    p = Progress(length(rhos) * length(nus); showspeed=true)
-    Threads.@threads for rho in rhos
-        Threads.@threads for nu in nus
-            rhostr = string(rho)
-            nustr = string(nu)
-            zetastr = replace(string(zeta), "." => "")
-            etastr = replace(string(eta), "." => "")
+    for s in ss
+        mkpath("$outdir/$s")
+    end
 
-            filename = "rho$(rhostr)_nu$(nustr)_gamma$(zetastr)_eta$(etastr)"
+    p = Progress(length(ss) * length(rhos) * length(nus); showspeed=true)
+    Threads.@threads for s in ss
+        Threads.@threads for rho in rhos
+            Threads.@threads for nu in nus
+                rhostr = string(rho)
+                nustr = string(nu)
+                zetastr = replace(string(zeta), "." => "")
+                etastr = replace(string(eta), "." => "")
 
-            if (
-                isfile("$outdir/$filename--history.csv") &&
-                isfile("$outdir/$filename--labels.csv") &&
-                isfile("$outdir/$filename--label_history.csv")
-            )
+                filename = "rho$(rhostr)_nu$(nustr)_gamma$(zetastr)_eta$(etastr)"
+
+                if (
+                    isfile("$outdir/$s/$filename--history.csv") &&
+                    isfile("$outdir/$s/$filename--labels.csv") &&
+                    isfile("$outdir/$s/$filename--label_history.csv")
+                )
+                    next!(p)
+                    continue
+                end
+
+                env, labels, label_history = run_normal_model(rho, nu, s; steps=20000)
+                history_df = DataFrame(;
+                    step=1:length(env.history), src=first.(env.history), dst=last.(env.history)
+                )
+                labels_df = DataFrame(; id=1:length(labels), label=labels)
+                label_history_df = DataFrame(label_history)
+
+                env = nothing
+                labels = nothing
+                label_history = nothing
+
+                CSV.write("$outdir/$s/$filename--history.csv", history_df)
+                CSV.write("$outdir/$s/$filename--labels.csv", labels_df)
+                CSV.write("$outdir/$s/$filename--label_history.csv", label_history_df)
+
+                history_df = nothing
+                labels_df = nothing
+                label_history_df = nothing
+
                 next!(p)
-                continue
             end
-
-            env, labels, label_history = run_normal_model(rho, nu; steps=20000)
-            history_df = DataFrame(;
-                step=1:length(env.history), src=first.(env.history), dst=last.(env.history)
-            )
-            labels_df = DataFrame(; id=1:length(labels), label=labels)
-            label_history_df = DataFrame(label_history)
-
-            env = nothing
-            labels = nothing
-            label_history = nothing
-
-            CSV.write("$outdir/$filename--history.csv", history_df)
-            CSV.write("$outdir/$filename--labels.csv", labels_df)
-            CSV.write("$outdir/$filename--label_history.csv", label_history_df)
-
-            history_df = nothing
-            labels_df = nothing
-            label_history_df = nothing
-
-            next!(p)
         end
     end
 end
